1d1d399605c655b955c2c562432d97cdc2806559
Sbaayel <baayel@gmail.com>
2020-05-15 21:01:57 -0400

    dog details

--- client/src/components/DogDetails.jsx
+++ client/src/components/DogDetails.jsx
@@ -2,7 +2,7 @@
 import React, { Component } from 'react'
 
 import '../App.css'
-import { getOneDog } from '../services/api-helper'
+import { getOneDog, destroyDog } from '../services/api-helper'
 import './DogDetails.css'
 class DogDetails extends Component {
   constructor(props) {
@@ -22,6 +22,11 @@ class DogDetails extends Component {
     this.setState({ dog })
   }
 
+  deleteDog(id) {
+    const deleteDog = this.state.deleteDog.filter( dog => dog.id !== id );
+    this.setState({deleteDog: destroyDog});
+  }
+
   render() {
     const { dog } = this.state;
     
@@ -29,20 +34,28 @@ class DogDetails extends Component {
       <div className="dog-details">
         {
           dog &&
+          
           <div className="detail">
-            <div className="name">{dog.name}</div>
-            <div className="age">{dog.age}</div>
-            <div className="sex">{dog.sex}</div>
-            <div className="breed">{dog.breed}</div>
-            <div className="weight">{dog.weight}</div>
-            <div className="height">{dog.height}</div>
-            <div className="shelter">{dog.shelter}</div>
-            <img className="image" src={dog.image} alt={dog.name}/>
-            <div className="description">{dog.description}</div>
+            <div className="image">
+              <img src={dog.image} alt={dog.name} />
+              <p>{dog.name}</p>
+            </div>
+            <div className="items">
+            
+            <p>Age: {dog.age}</p>
+            <p>Sex: {dog.sex}</p>
+            <p>Breed: {dog.breed}</p>
+            <p>Weight: {dog.weight}</p>
+            <p>Height: {dog.height}</p>
+            <p>Shelter: {dog.shelter}</p>
+            <p>{dog.description}</p>
+            </div>
 
+           
           </div>
         }
          
+        
       </div>
     )
   }
